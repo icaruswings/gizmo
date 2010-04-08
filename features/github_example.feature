523b5a5be37fadf50047926cbfba17631b8c511c
Luke Cunningham <luke.cunningham@rea-group.com>
2010-04-09 08:25:34 +1000

    modified cucumber scenario to reflect a user instead of a scenario

--- features/github_example.feature
+++ features/github_example.feature
@@ -3,10 +3,10 @@ Feature:
     In order to have a DRY, elegant and maintainable test suite
     I want 'gizmo' to do it's funky thing
 
-    Scenario: A scenario performs a github search
-        Given the scenario is on the github homepage
-        When the scenario enters "gizmo" into the search box
-        And the scenario clicks the magnifying glass icon
-        And the scenario clicks on the "icaruswings / gizmo" link
-        Then the scenario is on the "gizmo" github repository details page
-        And the scenario is on a github repository details page which belongs to "icaruswings"
+    Scenario: A user can perform a search from the homepage
+        Given a user is on the github homepage
+        When the user enters "gizmo" into the search box
+        And the user clicks the magnifying glass icon
+        And the user clicks on the "icaruswings / gizmo" link
+        Then the user is on the "gizmo" github repository details page
+        And the user is on a github repository details page which belongs to "icaruswings"
--- features/step_definitions/github_example_steps.rb
+++ features/step_definitions/github_example_steps.rb
@@ -1,36 +1,36 @@
-Given /^the scenario is on the github homepage$/ do
+Given /^a user is on the github homepage$/ do
   visit "http://github.com"
 end
 
-When /^the scenario enters "([^\"]*)" into the search box$/ do |text|
+When /^the user enters "([^\"]*)" into the search box$/ do |text|
   on_page_with :github_search do |page|
     fill_in page.search_form.input.attr('name').value, :with => text
   end
 end
 
-When /^the scenario clicks the magnifying glass icon$/ do
+When /^the user clicks the magnifying glass icon$/ do
   on_page_with :github_search do |page|
     click page.search_form.submit.attr('alt').value
   end
 end
 
-When /^the scenario clicks on the "([^\"]*)" link$/ do |text|
+When /^the user clicks on the "([^\"]*)" link$/ do |text|
   click_link text
 end
 
-Then /^the scenario is on a github repository details page$/ do
+Then /^the user is on a github repository details page$/ do
   on_page_with :github_repo_details do |page|
     page.should be_valid
   end
 end
 
-Then /^the scenario is on the "([^\"]*)" github repository details page$/ do |repo_name|
+Then /^the user is on the "([^\"]*)" github repository details page$/ do |repo_name|
   on_page_with :github_repo_details do |page|
     page.repo_details.name == repo_name
   end
 end
 
-Then /^the scenario is on a github repository details page which belongs to "([^\"]*)"$/ do |author|
+Then /^the user is on a github repository details page which belongs to "([^\"]*)"$/ do |author|
   on_page_with :github_repo_details do |page|
     page.repo_details.author == author
   end

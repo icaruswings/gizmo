e449efbd4dc4cb4711d018410d5969b6b746c37b
Luke Cunningham <luke.cunningham@rea-group.com>
2010-04-08 14:12:43 +1000

    added comment to explain Module override in extras.rb

--- lib/gizmo/extras.rb
+++ lib/gizmo/extras.rb
@@ -1,5 +1,8 @@
+# Override the #extended method on Module so it calls a method
+# #extended_with if it exists - we use this to manage an array
+# of modules that have been mixed in to a Gizmo::Page object
 class Module
-  def extended base
-    base.extended_with(self) if base.respond_to?(:extended_with)
+  def extended mixin
+    mixin.extended_with(self) if mixin.respond_to?(:extended_with)
   end
 end
